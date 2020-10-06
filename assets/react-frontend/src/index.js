9a594df2becd83f4dc1be01fe5f15cdf47e0062c
icewanee <iamicewanee@gmail.com>
2020-10-06 21:33:25 +0700

    add RegisterCard

--- assets/react-frontend/src/index.js
+++ assets/react-frontend/src/index.js
@@ -5,25 +5,15 @@ import {
   Switch,
   Route,
 } from 'react-router-dom';
-import "bootstrap/dist/css/bootstrap.min.css";
-import $ from "jquery";
-import Popper from "popper.js";
-import "bootstrap/dist/js/bootstrap.bundle.min";
+//import "bootstrap/dist/css/bootstrap.min.css";
+//import "bootstrap/dist/js/bootstrap.bundle.min";
 
 import Home from './pages/home';
 import RegisterCard from './components/RegisterCard';
-import RegisterPage from "./pages/RegisterPage";
 
 const App = () => {
   return (
-    <div> <RegisterCard /></div>
-    /*<Router>
-      <Switch>
-        <Route path="/">
-          <register />
-        </Route>
-      </Switch>
-    </Router>*/
+    <div><RegisterCard /></div>
   );
 };
 
