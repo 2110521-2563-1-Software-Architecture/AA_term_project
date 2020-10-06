8616140b322e9d69e4836aee713764a034bec5ce
wiwan5 <grace-2540@hotmail.com>
2020-10-06 20:32:15 +0700

    login +sign up

--- assets/react-frontend/src/index.js
+++ assets/react-frontend/src/index.js
@@ -1,27 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from 'react-router-dom';
+import React from 'react';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import Home from './pages/home';
-import Register  from './pages/register';
-import Navbar from './pages/nav-bar';
+import Login from "./components/login.js";
+import SignUp from "./components/signup.js";
 
+function App() {
+  return (<Router>
+    <div className="App">
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" to={"/sign-in"}>positronX.io</Link>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-in"}>Login</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
 
-const App = () => {
-  return (
-    <Router>
-      <Navbar/>
-      <Switch>
-        <Route path="/" component = {Home}/>
-        <Route path="/register" component = {Register} />
-
-      </Switch>
-    </Router>
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <Switch>
+            <Route exact path='/' component={Login} />
+            <Route path="/sign-in" component={Login} />
+            <Route path="/sign-up" component={SignUp} />
+          </Switch>
+        </div>
+      </div>
+    </div></Router>
   );
-};
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export default App;
--- assets/react-frontend/src/pages/register.js
+++ assets/react-frontend/src/pages/register.js
@@ -1,13 +1,14 @@
 import React from 'react';
 var config = require('../../config');
+import SignUp from '../components/signup.js'
 
-
-
-export const Register = () =>{
-      return(
-        <div>
-        <SignUp/>
-        </div>
-      )
+class Register extends React.Component {
+  render() {
+      return <div>
+          <h1>Hello, This is a test, backend at {config.backendLocation}</h1>
+          <SignUp/>
+      </div>;
+  }
 }
 
+export default Register
