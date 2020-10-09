b0e61e0bc15ba65375ccf9633f774b61d988229c
GreatZaNaRak <great2541@windowslive.com>
2020-10-09 22:57:52 +0700

    add navbar, install dependency

--- assets/react-frontend/src/index.js
+++ assets/react-frontend/src/index.js
@@ -7,10 +7,12 @@ import {
 } from 'react-router-dom';
 
 import Home from './pages/home';
+import Navbar from './pages/Navbar'
 
 const App = () => {
   return (
     <Router>
+      <Navbar />
       <Switch>
         <Route path="/" exact>
           <Home />
