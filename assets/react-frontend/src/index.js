d5b1034f9d8d1649903f0af95e0350703d391c44
icewanee <iamicewanee@gmail.com>
2020-10-14 19:36:40 +0700

    create register-page

--- assets/react-frontend/src/index.js
+++ assets/react-frontend/src/index.js
@@ -6,10 +6,11 @@ import {
   Route,
 } from 'react-router-dom';
 
-import Home from './pages/home';
+//import Home from './pages/home';
 import Navbar from './components/Navbar'
 import ShortenLinkPage from './pages/shorten-link-page';
 import SkipPage from './pages/skip-page';
+import RegisterCard from './pages/register-page';
 
 const App = () => {
   return (
@@ -21,6 +22,9 @@ const App = () => {
         </Route>
         <Route path="/ads" exact>
           <SkipPage />
+        </Route>
+        <Route path="/register" exact>
+          <RegisterCard />
         </Route>
       </Switch>
     </Router>
