c06522263bae67cd96e12ef1e925592cd7f4d794
GreatZaNaRak <great2541@windowslive.com>
2020-10-18 14:36:23 +0700

    change to styled components

--- assets/react-frontend/src/components/Navbar.js
+++ assets/react-frontend/src/components/Navbar.js
@@ -6,38 +6,38 @@ import Logo from '../../assets/logo.png'
 import styled from "styled-components"
 import "../styles.css"
 
+const NavbarWrapper = styled.div`
+font-family: Roboto;
+height: 64px;
+background-color: #2F4F4F;
+
+.element-wrapper {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  height: 64px;
+  margin-left: 3rem;
+  margin-right: 1rem;
+}
+
+.logo-img {
+  height: 40px;
+  font-family: sans-serif;
+  margin-left: 10;
+}
+
+@media (max-width: 650px) {
+  height: 96px;
+  .element-wrapper {
+    flex-direction: column;
+  }
+}
+`
+
 const Navbar = () => {
+  const [state, setState] = useState("user");
 
-  const NavbarWrapper = styled.div`
-    font-family: Roboto;
-    height: 64px;
-    background-color: #2F4F4F;
-
-    .element-wrapper {
-      display: flex;
-      flex-direction: row;
-      justify-content: space-between;
-      align-items: center;
-      height: 64px;
-      margin-left: 3rem;
-      margin-right: 1rem;
-    }
-
-    .logo-img {
-      height: 40px;
-      font-family: sans-serif;
-      margin-left: 10;
-    }
-
-    @media (max-width: 650px) {
-      height: 96px;
-      .element-wrapper {
-        flex-direction: column;
-      }
-    }
-  `
-
-  const [state, setState] = useState("guest");
   const selectState = (st) => {
     switch (st) {
       case "guest":
