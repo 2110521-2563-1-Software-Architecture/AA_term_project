9734175e1fb26c68d62886f1d608b81bd0d4b95b
GreatZaNaRak <great2541@windowslive.com>
2020-11-12 14:08:14 +0700

    complete structure of user profile component

--- assets/react-frontend/src/pages/userPage.js
+++ assets/react-frontend/src/pages/userPage.js
@@ -1,23 +1,54 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import { useHistory } from 'react-router-dom'
+import Axios from 'axios'
 
 import UserProfile from "../components/UserProfile"
 import EditUserProfile from "../components/EditUserProfile"
 
+const MainWrapper = styled.div`
+
+    display: flex;
+    justify-content: center;
+
+`
+
 const UserPage = () => {
 
+    const history = useHistory()
+
+    useEffect(() => {
+
+        if (!localStorage.getItem('token')) {
+            history.push('/')
+        }
+
+        const getUserInfo = async () => {
+
+            const JwtToken = `JWT ${localStorage.getItem('token')}`
+            let { data: { user: { email, name } } } = await Axios.get("http://aa-shortener.poomrokc.services/api/user/whoami", { headers: { Authorization: JwtToken } })
+
+            if (!name) {name = "Anonymous"}
+            
+            setUserInfo({email, name})
+
+        }
+
+        getUserInfo()
+    }, [])
+
     const [isEdit, setIsEdit] = useState(false)
-    const handle = () => {
-        setIsEdit(prevState => !prevState)
-    }
+    const [userInfo, setUserInfo] = useState({})
 
     return (
-        <div>
-            <h1>User</h1>
-            <button onClick={handle}>Click</button>
+        <MainWrapper>
             {
-                isEdit ?  <EditUserProfile /> : <UserProfile />
+                isEdit ?  
+                    <EditUserProfile onSet={setIsEdit} /> 
+                    : 
+                    <UserProfile onSet={setIsEdit} data={userInfo} />
             }
-        </div>
+        </MainWrapper>
     )
 }
 
